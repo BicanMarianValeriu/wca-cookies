5748ee7137b58852c7fd00991176b3a44a3fd0e2
Bican Marian Valeriu <marianvaleriubican@gmail.com>
2024-07-21 04:02:43 +0300

    aaa

--- class-cookies.php
+++ class-cookies.php
@@ -474,54 +474,54 @@ final class Cookies implements Integration {
 			}
 		}
 
-		$template = $p->get_updated_html();
-
-		\add_action( 'wp_footer', function() use ( $is_open, $template ): void {
-			// Toggler
-			$classnames = [ 'wp-element-button', 'wp-element-button--cookies' ];
-			$backdrop	= get_prop( $this->config, [ 'offcanvas', 'backdrop' ] );
-			
-			wecodeart_template( 'general/toggler', [
-				'id'		=> 'wp-cookies',
-				'class'		=> $classnames,
-				'toggle' 	=> 'offcanvas',
-				'icon'		=> [
-					'viewBox' 	=> '0 0 512 512',
-					'paths'		=> [
-						'M257.5 27.6c-.8-5.4-4.9-9.8-10.3-10.6v0c-22.1-3.1-44.6 .9-64.4 11.4l-74 39.5C89.1 78.4 73.2 94.9 63.4 115L26.7 190.6c-9.8 20.1-13 42.9-9.1 64.9l14.5 82.8c3.9 22.1 14.6 42.3 30.7 57.9l60.3 58.4c16.1 15.6 36.6 25.6 58.7 28.7l83 11.7c22.1 3.1 44.6-.9 64.4-11.4l74-39.5c19.7-10.5 35.6-27 45.4-47.2l36.7-75.5c9.8-20.1 13-42.9 9.1-64.9v0c-.9-5.3-5.3-9.3-10.6-10.1c-51.5-8.2-92.8-47.1-104.5-97.4c-1.8-7.6-8-13.4-15.7-14.6c-54.6-8.7-97.7-52-106.2-106.8zM208 144a32 32 0 1 1 0 64 32 32 0 1 1 0-64zM144 336a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm224-64a32 32 0 1 1 0 64 32 32 0 1 1 0-64z'
-					]
-				],
-				'options'	=> [
-					'isOpen' 	=> $is_open,
-					'scroll' 	=> filter_var( get_prop( $this->config, [ 'offcanvas', 'scroll' ] ), FILTER_VALIDATE_BOOLEAN ),
-					'keyboard' 	=> filter_var( get_prop( $this->config, [ 'offcanvas', 'keyboard' ] ), FILTER_VALIDATE_BOOLEAN ),
-					'backdrop'	=> $backdrop === 'static' ? 'static' : filter_var( $backdrop, FILTER_VALIDATE_BOOLEAN )
+		$markup = '';
+		// Toggler
+		$backdrop	= get_prop( $this->config, [ 'offcanvas', 'backdrop' ] );
+		
+		$markup .= wecodeart_template( 'general/toggler', [
+			'id'		=> 'wp-cookies',
+			'class'		=> [ 'wp-element-button', 'wp-element-button--cookies' ],
+			'toggle' 	=> 'offcanvas',
+			'icon'		=> [
+				'viewBox' 	=> '0 0 512 512',
+				'paths'		=> [
+					'M257.5 27.6c-.8-5.4-4.9-9.8-10.3-10.6v0c-22.1-3.1-44.6 .9-64.4 11.4l-74 39.5C89.1 78.4 73.2 94.9 63.4 115L26.7 190.6c-9.8 20.1-13 42.9-9.1 64.9l14.5 82.8c3.9 22.1 14.6 42.3 30.7 57.9l60.3 58.4c16.1 15.6 36.6 25.6 58.7 28.7l83 11.7c22.1 3.1 44.6-.9 64.4-11.4l74-39.5c19.7-10.5 35.6-27 45.4-47.2l36.7-75.5c9.8-20.1 13-42.9 9.1-64.9v0c-.9-5.3-5.3-9.3-10.6-10.1c-51.5-8.2-92.8-47.1-104.5-97.4c-1.8-7.6-8-13.4-15.7-14.6c-54.6-8.7-97.7-52-106.2-106.8zM208 144a32 32 0 1 1 0 64 32 32 0 1 1 0-64zM144 336a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm224-64a32 32 0 1 1 0 64 32 32 0 1 1 0-64z'
 				]
-			] );
+			],
+			'options'	=> [
+				'isOpen' 	=> $is_open,
+				'scroll' 	=> filter_var( get_prop( $this->config, [ 'offcanvas', 'scroll' ] ), FILTER_VALIDATE_BOOLEAN ),
+				'keyboard' 	=> filter_var( get_prop( $this->config, [ 'offcanvas', 'keyboard' ] ), FILTER_VALIDATE_BOOLEAN ),
+				'backdrop'	=> $backdrop === 'static' ? 'static' : filter_var( $backdrop, FILTER_VALIDATE_BOOLEAN )
+			]
+		], false );
 
-			// Offcanvas
-			$classnames = [
-				'wp-offcanvas--' . get_prop( $this->config, [ 'offcanvas', 'position' ] ),
-				'wp-offcanvas--cookies',
-			];
+		// Offcanvas
+		$classnames = [
+			'wp-offcanvas--' . get_prop( $this->config, [ 'offcanvas', 'position' ] ),
+			'wp-offcanvas--cookies',
+		];
 
-			$background = get_prop( $this->config, [ 'offcanvas', 'style', 'backgroundColor' ], '#ffffff' );
-			$background = wecodeart( 'styles' )::hex_to_rgb( $background, 1, true );
-			$luminance 	= wecodeart( 'styles' )::rgb_luminance( $background );
+		$background = get_prop( $this->config, [ 'offcanvas', 'style', 'backgroundColor' ], '#ffffff' );
+		$background = wecodeart( 'styles' )::hex_to_rgb( $background, 1, true );
+		$luminance 	= wecodeart( 'styles' )::rgb_luminance( $background );
 
-			$classnames[] = ( $luminance < get_lightness_limit() ) ? 'theme-is-dark' : 'theme-is-light';
+		$classnames[] = ( $luminance < get_lightness_limit() ) ? 'theme-is-dark' : 'theme-is-light';
 
-			if( $is_open ) {
-				$classnames[] = 'show';
-			}
+		if( $is_open ) {
+			$classnames[] = 'show';
+		}
 
-			wecodeart_template( 'general/offcanvas', [
-				'id'		=> 'wp-cookies',
-				'close'		=> get_prop( $this->config, [ 'offcanvas', 'close' ] ),
-				'class'		=> $classnames,
-				'title'		=> apply_filters( 'wecodeart/filter/support/cookies/offcanvas/title', get_prop( $this->config, [ 'offcanvas', 'title' ] ) ),
-				'content'	=> $template
-			] );
+		$markup .= wecodeart_template( 'general/offcanvas', [
+			'id'		=> 'wp-cookies',
+			'close'		=> get_prop( $this->config, [ 'offcanvas', 'close' ] ),
+			'class'		=> $classnames,
+			'title'		=> apply_filters( 'wecodeart/filter/support/cookies/offcanvas/title', get_prop( $this->config, [ 'offcanvas', 'title' ] ) ),
+			'content'	=> $p->get_updated_html()
+		], false );
+
+		\add_action( 'wp_footer', static function() use ( $markup ) {
+			echo $markup;
 		}, 0 );
 
 		if( ! is_null( $settings ) ) {
